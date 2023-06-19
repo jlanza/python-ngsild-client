cbe1e06e07081139cfdc60a89ff128a5f0f5c473
Jorge Lanza <jlanza@tlmat.unican.es>
2023-06-19 18:23:17 +0200

    Reformat document

--- src/ngsildclient/api/csourceregistrations.py
+++ src/ngsildclient/api/csourceregistrations.py
@@ -30,6 +30,7 @@ from ngsildclient.api.helper.csourceregistration import CSourceRegistration
 
 logger = logging.getLogger(__name__)
 
+
 class CSourceRegistrations:
     """A wrapper for the NGSI-LD API subscriptions endpoint."""
 
@@ -39,12 +40,16 @@ class CSourceRegistrations:
         self.url = url
 
     @rfc7807_error_handle
-    def register(self, csourcereg: CSourceRegistration, raise_on_conflict: bool = True) -> str:
+    def register(
+        self, csourcereg: CSourceRegistration, raise_on_conflict: bool = True
+    ) -> str:
         csourcereg_dict = csourcereg.to_dict()
         if raise_on_conflict:
             conflicts = self.conflicts(csourcereg_dict)
             if conflicts:
-                raise ValueError(f"A csource already exists with same target : {conflicts}")
+                raise ValueError(
+                    f"A csource already exists with same target : {conflicts}"
+                )
         r = self._session.post(f"{self.url}/", json=csourcereg_dict)
         self._client.raise_for_status(r)
         location = r.headers.get("Location")
@@ -56,33 +61,41 @@ class CSourceRegistrations:
         id_returned_from_broker = location.rsplit("/", 1)[-1]
         id = csourcereg_dict.get("id")
         if id is not None and id != id_returned_from_broker:
-            raise NgsiApiError(f"Broker returned wrong id. Expected={id} Returned={id_returned_from_broker}")
+            raise NgsiApiError(
+                f"Broker returned wrong id. Expected={id} Returned={id_returned_from_broker}"
+            )
         return id_returned_from_broker
 
     @rfc7807_error_handle
-    def query(self, id: list[str], 
-              type: str = None,
-              idPattern: str = None, 
-              attrs: str = None,
-              q: str = None,
-              csf: str = None,
-              geometry: str = None,
-              georel: str = None,
-              coordinates : str = None,
-              geoproperty : str = None,
-              timeproperty : str = None,
-              timerel  : str = None,
-              timeAt : str = None,
-              endTimeAt: str = None,
-              geometryProperty: str = None,
-              lang: str = None,
-              scopeQ: str = None,
-              ctx: str = CORE_CONTEXT) -> Sequence[CSourceRegistration]:
-
+    def query(
+        self,
+        id: list[str],
+        type: str = None,
+        idPattern: str = None,
+        attrs: str = None,
+        q: str = None,
+        csf: str = None,
+        geometry: str = None,
+        georel: str = None,
+        coordinates: str = None,
+        geoproperty: str = None,
+        timeproperty: str = None,
+        timerel: str = None,
+        timeAt: str = None,
+        endTimeAt: str = None,
+        geometryProperty: str = None,
+        lang: str = None,
+        scopeQ: str = None,
+        ctx: str = CORE_CONTEXT,
+    ) -> Sequence[CSourceRegistration]:
         if type is None and attrs is None and q is None and georel is None:
-            raise ValueError("At least one of type, attrs, q or georel must be provided")
+            raise ValueError(
+                "At least one of type, attrs, q or georel must be provided"
+            )
         if georel is not None and (geometry is None or coordinates is None):
-            raise ValueError("geometry and coordinates must be provided if georel is provided")
+            raise ValueError(
+                "geometry and coordinates must be provided if georel is provided"
+            )
 
         params = {}
         if isinstance(id, list) and len(id) > 0:
@@ -106,14 +119,23 @@ class CSourceRegistrations:
         if geoproperty:
             params["geoproperty"] = geoproperty
         if timeproperty:
-            if timeproperty not in ["observedAt", "createdAt", "modifiedAt", "deletedAt"]:
-                raise ValueError("timeproperty must be one of observedAt, createdAt, modifiedAt or deletedAt")
+            if timeproperty not in [
+                "observedAt",
+                "createdAt",
+                "modifiedAt",
+                "deletedAt",
+            ]:
+                raise ValueError(
+                    "timeproperty must be one of observedAt, createdAt, modifiedAt or deletedAt"
+                )
             params["timeproperty"] = timeproperty
         if timerel:
             if timerel not in ["before", "after", "between"]:
-                raise ValueError("timerel must be one of before, after or between")
+                raise ValueError(
+                    "timerel must be one of before, after or between"
+                )
             params["timerel"] = timerel
-        if timeAt: 
+        if timeAt:
             params["timeAt"] = timeAt
         if endTimeAt:
             params["endTimeAt"] = endTimeAt
@@ -129,7 +151,9 @@ class CSourceRegistrations:
             "Content-Type": None,
         }  # overrides session headers
         if ctx is not None:
-            headers["Link"] = f'<{ctx}>; rel="{CORE_CONTEXT}"; type="application/ld+json"'
+            headers[
+                "Link"
+            ] = f'<{ctx}>; rel="{CORE_CONTEXT}"; type="application/ld+json"'
         r = self._session.get(f"{self.url}", params=params, headers=headers)
         self._client.raise_for_status(r)
         csourceregistrations = r.json()
@@ -153,7 +177,9 @@ class CSourceRegistrations:
     @staticmethod
     def _hash(csource: dict):
         criteria = CSourceRegistrations._criteria_only(csource)
-        return sha1(json.dumps(criteria, sort_keys=True).encode("utf-8")).digest()
+        return sha1(
+            json.dumps(criteria, sort_keys=True).encode("utf-8")
+        ).digest()
 
     # TODO: Analyse what to include in the hash and how to query
     @rfc7807_error_handle
@@ -176,7 +202,9 @@ class CSourceRegistrations:
             "Content-Type": None,
         }  # overrides session headers
         if ctx is not None:
-            headers["Link"] = f'<{ctx}>; rel="{CORE_CONTEXT}"; type="application/ld+json"'
+            headers[
+                "Link"
+            ] = f'<{ctx}>; rel="{CORE_CONTEXT}"; type="application/ld+json"'
         r = self._session.get(f"{self.url}/{id}", headers=headers)
         self._client.raise_for_status(r)
         return r.json()
