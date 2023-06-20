d99699c4c7a152fc067601a4ac54d5e7afd3f94e
Jorge Lanza <jlanza@tlmat.unican.es>
2023-06-20 14:00:09 +0200

    Update authorship

--- src/ngsildclient/api/helper/subscription.py
+++ src/ngsildclient/api/helper/subscription.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 # Software Name: ngsildclient
-# SPDX-FileCopyrightText: Copyright (c) 2021 Orange
+# SPDX-FileCopyrightText: Copyright (c) 2021 Orange UNICAN
 # SPDX-License-Identifier: Apache 2.0
 #
 # This software is distributed under the Apache 2.0;
 # see the NOTICE file for more details.
 #
 # Author: Fabien BATTELLO <fabien.battello@orange.com> et al.
+# Author: Jorge LANZA <jlanza@tlmat.unican.es> et al.
 
 from dataclasses import dataclass
 
